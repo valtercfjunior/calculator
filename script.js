46185d99d8e7619afc1d9f61e9ef793277847332
Valter Junior <79730793+valtercfjunior@users.noreply.github.com>
2021-10-27 03:58:48 -0300

    Atualização
    botões
    innerHTML no display

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
-let banco = []
+let banco = ''
 const botao = document.querySelectorAll( '.numeroOperador')
 const botaoIgual = document.querySelector('.equal')
 const botaoLimpar = document.querySelector('.clear')
-const botaldel = document.querySelector('delete')
+const botaoDel = document.querySelector('.delete')
+const botaoPercent = document.querySelector('.percent')
+const displayOperation = document.querySelector('.operation')
+const displayResult = document.querySelector('.result')
+
+function atualizarOperation(banco){
+    displayOperation.innerHTML = banco
+}
 
 botao.forEach(item => {
     item.addEventListener('click', (e)=>{
-            banco.push(e.target.value)
-            console.log(banco)         
+            banco += e.target.value
+            console.log(banco)
+            atualizarOperation(banco)
+                  
     })
 })
 
 botaoIgual.addEventListener('click', (e)=>{
-    const resultado = eval(banco.toString().replaceAll(',', ''))
+    const resultado = eval(banco)
     console.log(resultado)
+    banco = resultado
+    displayResult.innerHTML = banco
+   
 
 })
 
+botaoDel.addEventListener('click', ()=>{
+    banco = banco.slice(0, banco.length - 1)
+    console.log(banco)
+    atualizarOperation(banco)
+    
+})
 
+botaoLimpar.addEventListener('click', ()=>{
+    banco = ''
+    displayResult.innerHTML = ''
+    console.log(banco)
+    atualizarOperation(banco)
+})
 
+botaoPercent.addEventListener('click', ()=>{
+    const resultado = eval(banco)
+    console.log(resultado)
+    banco = resultado
+})
+
+//validar: operator depois de operator;  dot depois de dot (dot em float)
